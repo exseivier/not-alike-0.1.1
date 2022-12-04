5cc07acc3b8ea2941153bee82f26437d92f7fd57
Exseivier_JMA_UAAAN <exseivier@users.noreply.github.com>
2022-12-04 11:58:50 -0600

    Add files via upload

---
+++ not_alike/nal.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python3
+import random
+import click
+#import utils as CMD
+import not_alike.utils as CMD
+
+
+@click.group()
+def main():
+    """
+        Not-Alike: Command pipeline that identifies dissimilar regions of a target genome by comparing it to a genomes database.
+    """
+    pass
+
+@main.command()
+@click.option('-g', '--genome', \
+                help = 'Query genome FASTA file name', \
+                required = True, \
+                type = str)
+@click.option('-ws', '--window-size', \
+                help = 'Fragments from split genome will size equal to window size', \
+                required = True, \
+                type = int)
+@click.option('-ss', '--step-size', \
+                help = 'Step size in nucleotides the window takes between each cut along the genome', \
+                required = True, \
+                type = int)
+@click.option('-db', '--database-file', \
+                help = 'A name of a file that contains the pathway to BLAST_DB ver5 (*.db) files separated by return character', \
+                required = True, \
+                type = str)
+@click.option('-e', '--evalue', \
+                help = 'E-value cutoff', \
+                required = True, \
+                type = str)
+@click.option('-i', '--identity', \
+                help = 'Identity percentage cutoff', \
+                required = True, \
+                type = str)
+@click.option('-q', '--qcov', \
+                help = 'HSP query coverage cutoff', \
+                required = True, \
+                type = str)
+@click.option('-t', '--task', \
+                help = 'BLAST task [blastn | megablast | dc-megablast]', \
+                required = True, \
+                type = str)
+@click.option('-c', '--comment', \
+                help = 'Leave a comment enclosed by single quotes', \
+                required = False, \
+                default = 'Empty', \
+                type = str)
+def search(genome, window_size, step_size, database_file, evalue, identity, qcov, task, comment):
+    """
+        Searches for not alike fragments in query genome
+    """
+    PID = random.randrange(1, 9999999999)
+    out_split = '.'.join(genome.split('.')[:-1]) + '_split_' + str(window_size) + '_' + str(step_size) + '.fasta'
+    out_split = ''.join(out_split.split('/')[-1])
+    input_split = 'input_split.' + str(PID) + '.fasta'
+    print(out_split + ' was loaded.')
+    print(input_split + ' was loaded.')
+
+    CMD.check_path_exists('split_out')
+    CMD.check_path_exists('blast_db')
+    CMD.check_path_exists('blast_out')
+    CMD.check_path_exists('ht2_idx')
+    CMD.check_path_exists('mapping')
+    CMD.check_path_exists('gtfs')
+    CMD.check_path_exists('log')
+
+    CMD.loggin_data(PID, genome, database_file, window_size, step_size, task, identity, qcov, evalue, comment)
+    
+    out_split = 'split_out/' + out_split
+    input_split = 'split_out/' + input_split
+    if not CMD.os.path.exists(out_split):
+        CMD.split_genome(genome, window_size, step_size, out_split)
+    else:
+        print('A split-genome file was found!!!')
+
+    CMD.copy_file(out_split, input_split)
+
+    db_files = CMD.load_lines(database_file)
+
+    for f in db_files:
+        dbf = '.'.join(f.split('.')[:-1]) + '.db'
+        print('Blasting ' + dbf + ' ...')
+        CMD.do_blast(input_split, 'blast_db/' + dbf, 'blast_out/out.blast', evalue, identity, qcov, task)
+        print('Updating input_split')
+        CMD.select_sequences(input_split, 'blast_out/out.blast')
+
+    print('BLASTn searching done!')
+    print('Mapping on process.')
+    
+    CMD.mapping(genome, input_split)
+
+    print('Assembly on process.')
+    CMD.assembly(PID)
+
+    print('Extracting sequences.')
+    CMD.extseq(genome, PID)
+
+    print('not-alike has finished.')
+
+@main.command()
+@click.option('-db', '--db-path', \
+                help = 'Path to FASTA files database', \
+                required = True, \
+                type = str)
+def database_makeblastdb(db_path):
+    """
+        Builds a BLAST_DB (version 5) database files
+    """
+
+    CMD.make_db(db_path)
+    
+@main.command()
+@click.option('-db', '--db-path', \
+                help = 'Path to FASTA files database', \
+                required = True, \
+                type = str)
+def database_makefiledb(db_path):
+    """
+        Creates the database text file which contains the BLAST_DB files paths.
+    """
+    CMD.make_txtfiledb(db_path)
+
+
+@main.command()
+@click.option('-p', '--db-path', \
+                help = 'Genomes database path', \
+                required = True, \
+                type = str)
+def show_database(db_path):
+    """
+        Shows metadata of database [accession number, organism name and organism taxon id]
+    """
+
+    assembly_report = db_path + '/assembly_data_report.jsonl'
+    assembly_report_tsv = db_path + '/assembly_data_report.tsv'
+    if not CMD.file_exists(assembly_report_tsv):
+        CMD.dataformat_tsv(assembly_report, assembly_report_tsv)
+
+    print(assembly_report_tsv)
+
+    CMD.print_table(assembly_report_tsv)
+        
+@main.command()
+def show_exp():
+    """
+        Shows information about epxeriments stored in the current working directory
+    """
+    CMD.show_exp_info()
+
+
+if __name__ == '__main__':
+    main()
---
+++ not_alike/utils.py
@@ -0,0 +1,517 @@
+import os
+import subprocess as sup
+import pandas as pd
+
+
+########################################
+######                            ######
+######      HELPER FUNCTIONS      ######
+######                            ######
+########################################
+
+
+######      HELPER FUNCTION FOR SPLIT-GENOME
+
+def assert_directory(path):
+    """
+        Asserts directory exists
+    """
+    path = path.split('/')
+    file_exists = os.path.exists('/'.join(path))
+    if file_exists:
+        print('File exists')
+    else:
+        print('File does not exists!!!!')
+        print('Creating directory and / or file')
+        if len(path) > 1:
+            directory = ''.join(path[:-1])
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+            else:
+                pass
+            path = '/'.join([directory, path[-1]])
+            with open(path, 'w') as fh:
+                fh.close()
+        else:
+            with open(''.join(path), 'w') as fh:
+                fh.close()
+
+def __load_seqs(filename):
+    """
+
+    """
+    FHIN = open(filename, "r")
+
+    line = FHIN.readline()
+    seqs = {}
+    while line:
+        line = line.strip("\n")
+        if line[0] == ">":
+            header = line
+            seqs[header] = []
+        else:
+            seqs[header].append(line)
+
+        line = FHIN.readline()
+    out_seqs = {}
+    for head, seq in seqs.items():
+        out_seqs[head] = "".join(seq)
+
+    FHIN.close()
+    seqs = None
+    return out_seqs
+
+
+def load_lines(infile):
+    """
+        Loads lines from file and stores them in a list
+    """
+    lines = []
+    with open(infile, 'r') as fh:
+        for line in fh:
+            lines.append(line.strip())
+        
+        fh.close()
+
+    return lines
+
+def __split(seqs, size, step):
+    """
+
+    """
+    out_seqs = {}
+    seq_counter = 0
+    for head, seq in seqs.items():
+        lseq = len(seq)
+        i = 0
+        start = i
+        while start < lseq:
+            end = start + size - 1
+            out_seqs[">fragment_" + str(seq_counter)] = seq[start:end]
+            start = start + step
+            i += 1
+            seq_counter += 1
+
+    return out_seqs
+
+def __write_seqs(seqs, outfile):
+    """
+
+    """
+    FHOUT = open(outfile, "w+")
+    
+    big_string = ""
+    for head, seq in seqs.items():
+        big_string += head + "\n" + seq + "\n"
+
+    FHOUT.write(big_string)
+    FHOUT.close()
+
+##################################################
+
+######      HELPER FUNCTION FOR SELECT-SEQUENCES
+
+def __load_headers(hd_file):
+    """
+        Load the headers of the sequences that hitted a subject in genomes database.
+    """
+    
+    FHIN = open(hd_file, "r")
+
+    line = FHIN.readline()
+    heads = []
+    while line:
+
+        line = line.strip("\n")
+        heads.append(line)
+        line = FHIN.readline()
+
+    FHIN.close()
+    return heads
+
+
+def __select_seqs(seqs, heads):
+    """
+        Selects those sequences which header is not in heads list.
+    """
+
+    lsq = len(seqs)
+    print(str(lsq) + ' current sequences')
+    heads = list(set(heads))
+    lhd = len(heads)
+    print(str(lhd) + ' dropped secuences')
+    if lhd <= 0:
+        return seqs
+    list_seqs_keys = [x[1:] for x in list(seqs.keys())]
+    list_seqs_keys = list(set(list_seqs_keys))
+
+    heads_seqs_keys = list_seqs_keys + heads
+    heads_seqs_keys = sorted(heads_seqs_keys)
+    
+    pivot = 0
+    nextp = pivot + 1
+
+    # Modified section for version 0.1.1
+
+    while pivot < len(heads_seqs_keys)-1:
+        if heads_seqs_keys[pivot] == heads_seqs_keys[nextp]:
+            heads_seqs_keys.pop(pivot)
+            heads_seqs_keys.pop(pivot)
+        else:
+            pivot += 1
+            nextp = pivot + 1
+    
+
+    out_seqs = {}
+
+    for head in heads_seqs_keys:
+        head = '>' + head
+        out_seqs[head] = seqs[head]
+
+    #####################################
+
+    print(str(len(out_seqs)) + ' maintained sequences')
+    return out_seqs
+
+#####################################################################
+
+def file_exists(infile):
+    """
+        Checks if path is a regular file
+    """
+
+    if os.path.exists(infile) and os.path.isfile(infile):
+        return True
+    else:
+        return False
+
+def check_path_exists(path):
+    """
+        Checks path exist
+    """
+    if not os.path.exists(path):
+        os.makedirs(path)
+    else:
+        print(path + ' folder exists!')
+
+
+def copy_file(source, destiny):
+    """
+        Copies a file
+    """
+    p = sup.Popen(['cp', source, destiny], stdout = sup.PIPE, stderr = sup.PIPE)
+    p.communicate()
+    p.kill()
+    if os.path.exists(destiny):
+        print(source + ' was copied to ' + destiny)
+    else:
+        print('Copy process | Error!!!')
+
+def rm_file(path):
+    """
+        Removes the file specifyied in path
+    """
+    if os.path.exists(path):
+        os.remove(path)
+    else:
+        print(path + ' not found.')
+
+
+######      HELPER FUNCTIONS FOR MAPPPING
+
+
+def __ht2idx_ready():
+    """
+        Checks if Hisat2 database is allready formated.
+    """
+
+    for fname in os.listdir('ht2_idx'):
+        if fname.endswith('.1.ht2'):
+            return True
+        
+    return False
+
+
+def __index(ref_genome, fname_suffix):
+    """
+        Prepare an index of reference genome (a.k.a. query genome)
+    """
+
+    p = sup.Popen(['hisat2-build', \
+                ref_genome, \
+                'ht2_idx/' + fname_suffix], \
+                stdout = sup.PIPE, \
+                stderr = sup.PIPE)
+    p.communicate()
+    p.kill()
+
+
+def __map(ref_genome, input_split, fname_suffix):
+    """
+        Maps sequences to reference genome
+    """
+
+    pid = input_split.split('.')[-2]
+    mapping_out_sam = 'mapping/nal_frags.' + pid + '.sam'
+    mapping_out_bam = 'mapping/nal_frags.' + pid + '.bam'
+    mapping_out_sbam = 'mapping/nal_frags.' + pid + '.sort.bam'
+    p = sup.Popen(['hisat2', \
+                '-x', 'ht2_idx/' + fname_suffix, \
+                '--no-temp-splicesite', \
+                '--no-spliced-alignment', \
+                '-f', \
+                '-U', input_split, \
+                '-S', mapping_out_sam], \
+                stdout = sup.PIPE, \
+                stderr = sup.PIPE)
+
+    p.communicate()
+    p.kill()
+    p = sup.Popen(['samtools', 'view', \
+                '-b', '-h', \
+                '-f', '0x0', '-F', '0x100', \
+                '-o', mapping_out_bam, \
+                mapping_out_sam], \
+                stdout = sup.PIPE, \
+                stderr = sup.PIPE)
+    p.communicate()
+    p.kill()
+    p = sup.Popen(['samtools', 'sort', \
+                '-o', mapping_out_sbam, \
+                '-O', 'BAM', \
+                '--reference', ref_genome, \
+                mapping_out_bam], \
+                stdout = sup.PIPE, \
+                stderr = sup.PIPE)
+    p.communicate()
+    p.kill()
+
+#########################################################
+
+######      HELPER FUNCTION FOR ASSEMBLY
+
+def __do_assembly(pid):
+    p = sup.Popen(['stringtie', 'mapping/nal_frags.' + str(pid) + '.sort.bam', \
+                    '-L', '-s', '1', '-g', '50', \
+                    '-o', 'gtfs/nal_frags.' + str(pid) + '.gtf'], \
+                    stdout = sup.PIPE, \
+                    stderr = sup.PIPE)
+
+    p.communicate()
+    p.kill()
+
+
+##########################################################
+
+
+######      HELPER FUNCTION FOR EXTSEQ
+
+def __extract_sequences(genome, pid):
+    tmp_gff_file = 'gtfs/tmp.gff'
+    fasta_out = 'gtfs/nal_frags.' + str(pid) + '.fasta'
+    p = sup.Popen(['gffread', 'gtfs/nal_frags.' + str(pid) + '.gtf', \
+                    '-o', tmp_gff_file], \
+                    stdout = sup.PIPE, \
+                    stderr = sup.PIPE)
+    p.communicate()
+    p.kill()
+
+    p = sup.Popen(['gffread', tmp_gff_file, \
+                    '-g', genome, \
+                    '-w', fasta_out], \
+                    stdout = sup.PIPE, \
+                    stderr = sup.PIPE)
+    p.communicate()
+    p.kill()
+
+
+
+######################################
+######                          ######
+######      MAIN FUNCTIONS      ######
+######                          ######
+######################################
+
+
+def split_genome(in_file, size, step_size, out_file):
+    """
+        Split query genome in fragments of determined size and at each determined step
+    """
+    print("Loading genome...")
+    seqs = __load_seqs(in_file)
+    print("Splitting genome...")
+    seqs = __split(seqs, size, step_size)
+    print("Writting to file...")
+    __write_seqs(seqs, out_file)
+
+    print(f"Spliting genome {in_file}")
+
+
+def do_blast(query, db_file, out_blast, evalue, idt, qcov, task):
+    """
+        Performs a BLASTn task.
+    """
+    
+    p = sup.Popen(['blastn', \
+                    '-query', query, \
+                    '-db', db_file, \
+                    '-out', out_blast, \
+                    '-outfmt', '6 qseqid', \
+                    '-task', task, \
+                    '-perc_identity', idt, \
+                    '-qcov_hsp_perc', qcov, \
+                    '-evalue', evalue, \
+                    '-max_target_seqs', str(1)], \
+                    stdout = sup.PIPE, \
+                    stderr = sup.PIPE)
+
+    p.communicate()
+    p.kill()
+
+def select_sequences(in_file, hd_file):
+    """
+        Selects those sequences that hit a subject in blast searching
+    """
+    
+    seqs = __load_seqs(in_file)
+    
+    heads = __load_headers(hd_file)
+
+    seqs = __select_seqs(seqs, heads)
+
+    __write_seqs(seqs, in_file)
+
+
+def mapping(ref_genome, input_split):
+    """
+
+    """
+    fname_suffix = ref_genome.split('/')[-1]
+    fname_suffix = '.'.join(fname_suffix.split('.')[:-1])
+
+    if not __ht2idx_ready():
+        print('ht2 index not found.')
+        print('Indexing.')
+        __index(ref_genome, fname_suffix)
+    else:
+        print('ht2 index found.')
+    
+    print('Mapping to reference genome.')
+    __map(ref_genome, input_split, fname_suffix)
+    print('Mapping finished.')
+
+
+
+def assembly(pid):
+    """
+        Assembles fragments of query genome using the genome-guided procedure.
+    """
+
+    __do_assembly(pid)
+
+def extseq(genome, PID):
+    """
+        Extracts sequences from genome using gff or gtf input file.
+    """
+    __extract_sequences(genome, PID)
+
+def dataformat_tsv(assembly_report, assembly_report_tsv):
+    """
+        Transforms jsonl to tsv format from ncbi dataset
+    """
+#    print(assembly_report)
+#    print(assembly_report_tsv)
+    fields = 'accession,organism-name,organism-tax-id'
+    with open(assembly_report_tsv, 'w') as FHOUT:
+        p = sup.Popen(['dataformat', 'tsv', 'genome', \
+                        '--inputfile', assembly_report, \
+                        '--fields', fields, \
+                        '--force'],
+                        stdout = FHOUT,
+                        stderr = sup.PIPE)
+        out, err = p.communicate()
+#        print('hello world! ' + str(len(err)))
+        p.kill()
+        FHOUT.close()
+
+def make_db(db_path):
+    """
+        Formats FASTA files to BLAST_DB format
+    """
+    
+    dataset_catalog = db_path + '/dataset_catalog.json'
+    assemblies = pd.read_json(dataset_catalog)
+    assemblies = assemblies['assemblies']
+    for assembly in assemblies:
+        for fastafile in assembly['files']:
+            if fastafile['fileType'] == 'GENOMIC_NUCLEOTIDE_FASTA':
+                inputfile = db_path + '/' + fastafile['filePath']
+                outputfile = '.'.join(inputfile.split('.')[:-1]) + '.db'
+                p = sup.Popen(['makeblastdb', \
+                            '-in', inputfile, \
+                            '-dbtype', 'nucl', \
+                            '-parse_seqids', \
+                            '-out', outputfile],
+                            stdout = sup.PIPE,
+                            stderr = sup.PIPE)
+                stdout, stderr = p.communicate()
+                print(stdout)
+                print(stderr)
+                p.kill()
+
+def loggin_data(pid, genome, database_file, window_size, step_size, task, identity, qcov, evalue, comment):
+    """
+        Stores experiment information in a log.
+    """
+    date = pd.to_datetime('today')
+    date = '-'.join([str(date.year), str(date.month), str(date.day)])
+    if not file_exists('log/experiments.log'):
+        with open('log/experiments.log', 'a') as FHIN:
+            FHIN.write('PID\tQSEQ\tDB_FILE\tWS\tSS\tTASK\tIDENT\tQCOV\tEVAL\tCOMMENT\tDATE\n')
+            FHIN.write(str(pid) + '\t' \
+                        + genome + '\t' \
+                        + database_file + '\t' \
+                        + str(window_size) + '\t' \
+                        + str(step_size) + '\t' \
+                        + task + '\t' \
+                        + str(identity) + '\t' \
+                        + str(qcov) + '\t' \
+                        + str(evalue) + '\t' \
+                        + comment + '\t' \
+                        + date + '\n')
+            FHIN.close()
+    else:
+        with open('log/experiments.log', 'a') as FHIN:
+            FHIN.write(str(pid) + '\t' \
+                        + genome + '\t' \
+                        + database_file + '\t' \
+                        + str(window_size) + '\t' \
+                        + str(step_size) + '\t' \
+                        + task + '\t' \
+                        + str(identity) + '\t' \
+                        + str(qcov) + '\t' \
+                        + str(evalue) + '\t' \
+                        + comment + '\t' \
+                        + date + '\n')
+            FHIN.close()
+
+def print_table(tsv_file):
+    """
+        Prints on screen a pandas data frame.
+    """
+
+    tbl = pd.read_table(tsv_file, sep = '\t', header = None)
+    print(tbl)
+
+def show_exp_info():
+    """
+        Prints on screen log/experiments.log file content.
+    """
+    
+    if os.path.exists('log/'):
+        tbl = pd.read_table('log/experiments.log', sep = '\t')
+        tbl.isetitem(10, [pd.Timestamp(x) for x in tbl.loc[:, 'DATE']])
+        print(tbl)
+    else:
+        print('Unable to find log/ folder.')
+
